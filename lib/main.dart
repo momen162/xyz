04064dc3b59f91b3799c9b6936282d3779484291
momen162 <miah15-4789@diu.edu.bd>
2025-04-14 15:14:19 +0600

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,8 @@ class MyApp extends StatelessWidget {
         // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
     
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        
+        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
